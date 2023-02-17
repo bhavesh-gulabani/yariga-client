f33f736032293a9e1a7f34a76e95304372d7d756
bhavesh-gulabani <bhaveshgulabani1@gmail.com>
2023-02-17 15:27:01 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-import { Refine, AuthProvider } from "@pankod/refine-core";
+import { Refine, AuthProvider } from '@pankod/refine-core';
 import {
   notificationProvider,
   RefineSnackbarProvider,
@@ -8,23 +6,40 @@ import {
   GlobalStyles,
   ReadyPage,
   ErrorComponent,
-} from "@pankod/refine-mui";
+} from '@pankod/refine-mui';
+import {
+  AccountCircleOutlined,
+  ChatBubbleOutline,
+  PeopleAltOutlined,
+  StarOutlineRounded,
+  VillaOutlined,
+} from '@mui/icons-material';
 
-import dataProvider from "@pankod/refine-simple-rest";
-import { MuiInferencer } from "@pankod/refine-inferencer/mui";
-import routerProvider from "@pankod/refine-react-router-v6";
-import axios, { AxiosRequestConfig } from "axios";
-import { ColorModeContextProvider } from "contexts";
-import { Title, Sider, Layout, Header } from "components/layout";
-import { Login } from "pages/login";
-import { CredentialResponse } from "interfaces/google";
-import { parseJwt } from "utils/parse-jwt";
+import dataProvider from '@pankod/refine-simple-rest';
+import routerProvider from '@pankod/refine-react-router-v6';
+import axios, { AxiosRequestConfig } from 'axios';
+import { ColorModeContextProvider } from 'contexts';
+import { Title, Sider, Layout, Header } from 'components/layout';
+import { CredentialResponse } from 'interfaces/google';
+import { parseJwt } from 'utils/parse-jwt';
+
+import {
+  Login,
+  Home,
+  Agents,
+  MyProfile,
+  PropertyDetails,
+  AllProperties,
+  CreateProperty,
+  AgentProfile,
+  EditProperty,
+} from 'pages';
 
 const axiosInstance = axios.create();
 axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   if (request.headers) {
-    request.headers["Authorization"] = `Bearer ${token}`;
+    request.headers['Authorization'] = `Bearer ${token}`;
   } else {
     request.headers = {
       Authorization: `Bearer ${token}`,
@@ -36,29 +51,49 @@ axiosInstance.interceptors.request.use((request: AxiosRequestConfig) => {
 
 function App() {
   const authProvider: AuthProvider = {
-    login: ({ credential }: CredentialResponse) => {
+    login: async ({ credential }: CredentialResponse) => {
       const profileObj = credential ? parseJwt(credential) : null;
 
+      // Save user to MongoDB...
       if (profileObj) {
-        localStorage.setItem(
-          "user",
-          JSON.stringify({
-            ...profileObj,
-            avatar: profileObj.picture,
-          })
+        const response = await fetch(
+          'https://yariga-dashboard-yqe4.onrender.com/api/v1/users',
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              name: profileObj.name,
+              email: profileObj.email,
+              avatar: profileObj.picture,
+            }),
+          }
         );
+
+        const data = await response.json();
+
+        if (response.status === 200) {
+          localStorage.setItem(
+            'user',
+            JSON.stringify({
+              ...profileObj,
+              avatar: profileObj.picture,
+              userid: data._id,
+            })
+          );
+
+          localStorage.setItem('token', `${credential}`);
+        } else {
+          return Promise.reject();
+        }
       }
-
-      localStorage.setItem("token", `${credential}`);
-
       return Promise.resolve();
     },
     logout: () => {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token');
 
-      if (token && typeof window !== "undefined") {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
+      if (token && typeof window !== 'undefined') {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
         axios.defaults.headers.common = {};
         window.google?.accounts.id.revoke(token, () => {
           return Promise.resolve();
@@ -69,7 +104,7 @@ function App() {
     },
     checkError: () => Promise.resolve(),
     checkAuth: async () => {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token');
 
       if (token) {
         return Promise.resolve();
@@ -79,7 +114,7 @@ function App() {
 
     getPermissions: () => Promise.resolve(),
     getUserIdentity: async () => {
-      const user = localStorage.getItem("user");
+      const user = localStorage.getItem('user');
       if (user) {
         return Promise.resolve(JSON.parse(user));
       }
@@ -89,21 +124,47 @@ function App() {
   return (
     <ColorModeContextProvider>
       <CssBaseline />
-      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: 'auto' } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          dataProvider={dataProvider(
+            'https://yariga-dashboard-yqe4.onrender.com/api/v1'
+          )}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
           resources={[
             {
-              name: "posts",
-              list: MuiInferencer,
-              edit: MuiInferencer,
-              show: MuiInferencer,
-              create: MuiInferencer,
-              canDelete: true,
+              name: 'properties',
+              list: AllProperties,
+              show: PropertyDetails,
+              create: CreateProperty,
+              edit: EditProperty,
+              icon: <VillaOutlined />,
+            },
+            {
+              name: 'agents',
+              list: Agents,
+              show: AgentProfile,
+              icon: <PeopleAltOutlined />,
+            },
+            {
+              name: 'reviews',
+              list: Home,
+              icon: <StarOutlineRounded />,
+            },
+            {
+              name: 'messages',
+              list: Home,
+              icon: <ChatBubbleOutline />,
+            },
+            {
+              name: 'my-profile',
+              options: {
+                label: 'My Profile',
+              },
+              list: MyProfile,
+              icon: <AccountCircleOutlined />,
             },
           ]}
           Title={Title}
@@ -113,6 +174,7 @@ function App() {
           routerProvider={routerProvider}
           authProvider={authProvider}
           LoginPage={Login}
+          DashboardPage={Home}
         />
       </RefineSnackbarProvider>
     </ColorModeContextProvider>
